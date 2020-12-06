5ae34e2940d6d61498e935b9d19493e5624561e4
ricpar90 <ricardo_pardo1990@hotmail.com>
2020-12-06 18:20:03 +0100

    galeria

--- htdocs/Tcuido/instalaciones.php
+++ htdocs/Tcuido/instalaciones.php
@@ -6,7 +6,7 @@
 <!-- seccion -->
 
 <section class="seccion contenedor">
-    <h2>Instalaciones</h2>
+    <h2>Galería de fotos</h2>
     <div class="instalaciones">
         <a href="img/galeria/01.jpg" data-lightbox="galeria">
             <img src="img/galeria/thumbs/01.jpg" alt="">
@@ -37,6 +37,48 @@
         </a>
         <a href="img/galeria/10.jpg" data-lightbox="galeria">
             <img src="img/galeria/thumbs/10.jpg" alt="">
+        </a>
+        <a href="img/galeria/11.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/11.jpg" alt="">
+        </a>
+        <a href="img/galeria/12.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/12.jpg" alt="">
+        </a>
+        <a href="img/galeria/13.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/13.jpg" alt="">
+        </a>
+        <a href="img/galeria/14.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/14.jpg" alt="">
+        </a>
+        <a href="img/galeria/15.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/15.jpg" alt="">
+        </a>
+        <a href="img/galeria/16.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/16.jpg" alt="">
+        </a>
+        <a href="img/galeria/17.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/17.jpg" alt="">
+        </a>
+        <a href="img/galeria/18.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/18.jpg" alt="">
+        </a>
+        <a href="img/galeria/19.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/19.jpg" alt="">
+        </a>
+        <a href="img/galeria/20.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/20.jpg" alt="">
+        </a>
+        <a href="img/galeria/21.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/21.jpg" alt="">
+        </a>
+        <a href="img/galeria/22.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/22.jpg" alt="">
+        </a>
+        <a href="img/galeria/23.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/23.jpg" alt="">
+        </a>
+        <a href="img/galeria/24.jpg" data-lightbox="galeria">
+            <img src="img/galeria/thumbs/24.jpg" alt="">
         </a>
     </div>
 </section>
