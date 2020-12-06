db1f30670ee25a82a39dd15d987b56e3b50b31a3
ricpar90 <ricardo_pardo1990@hotmail.com>
2020-12-06 18:20:24 +0100

    registro

--- htdocs/Tcuido/registro.php
+++ htdocs/Tcuido/registro.php
@@ -2,7 +2,7 @@
 <!--barra-->
 
 <section class="seccion contenedor">
-    <h2>PEDIDOS ONLINE</h2>
+    <h2>CITA ON-LINE</h2>
     <form id="registro" class="registro" action="validar_registro.php" method="post">
         <div id="datos_cliente" class="registro caja clearfix">
             <div class="campo">
@@ -83,112 +83,113 @@
         <!--paquetes-->
 
         <div id="eventos" class="eventos clearfix">
-            <h3>Busca tu producto</h3>
+            <h3>Escoge día y hora</h3>
             <div class="caja">
                 <div id="estetica" class="contenido-producto clearfix">
                     <h4>Estética</h4>
                     <div>
-                        <p>Consulta 1</p>
-                        <label><input type="checkbox" name="registro[]" id="est_01" value="est_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="est_02" value="est_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="est_03" value="est_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="est_04" value="est_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="est_05" value="est_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Lunes</p>
+                        <label><input type="checkbox" name="registro[]" id="est_01" value="est_01"><a>09:50h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="est_02" value="est_02"><a>10:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="est_03" value="est_03"><a>11:15h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="est_04" value="est_04"><a>12:45h </a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="est_05" value="est_05"><a>13:50h </a> Limpieza de Cutis</label>
                     </div>
                     <div>
-                        <p>Consulta 2:</p>
-                        <label><input type="checkbox" name="registro[]" id="est_06" value="est_06"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="est_07" value="est_07"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="est_08" value="est_08"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="est_09" value="est_09"><price>55€ - 300€ </price> Estética facial</label>
+                        <p>Martes</p>
+                        <label><input type="checkbox" name="registro[]" id="est_06" value="est_06"><a>09:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="est_07" value="est_07"><a>10:20h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="est_08" value="est_08"><a>11:50h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="est_09" value="est_09"><a>13:10h </a> Estética facial</label>
                     </div>
                     <div>
-                        <p>Consulta est:</p>
-                        <label><input type="checkbox" name="registro[]" id="salaest_01" value="salaest_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="salaest_02" value="salaest_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="salaest_03" value="salaest_03"><price>35€ - 175€ </price> Estética Corporal</label>
+                        <p>Miércoles</p>
+                        <label><input type="checkbox" name="registro[]" id="salaest_01" value="salaest_01"><a>10:40h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="salaest_02" value="salaest_02"><a>11:50h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="salaest_03" value="salaest_03"><a>15:30h </a> Estética Corporal</label>
                         
                     </div>
+                
                 </div>
                 <!--#estetica-->
                 <div id="opi" class="contenido-producto clearfix">
                     <h4>OPI</h4>
                     <div>
-                        <p>Consulta 1</p>
-                        <label><input type="checkbox" name="registro[]" id="opi_01" value="opi_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_02" value="opi_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_03" value="opi_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_04" value="opi_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_05" value="opi_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Lunes</p>
+                        <label><input type="checkbox" name="registro[]" id="opi_01" value="opi_01"><a>09:25h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_02" value="opi_02"><a>10:35h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_03" value="opi_03"><a>11:50h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_04" value="opi_04"><a>12:40h </a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_05" value="opi_05"><a>13:50h </a> Limpieza de Cutis</label>
                     </div>
                     <div>
-                        <p>Consulta 2:</p>
-                        <label><input type="checkbox" name="registro[]" id="opi_06" value="opi_06"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_07" value="opi_07"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_08" value="opi_08"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="opi_09" value="opi_09"><price>55€ - 300€ </price> Estética facial</label>
+                        <p>Martes</p>
+                        <label><input type="checkbox" name="registro[]" id="opi_06" value="opi_06"><a>09:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_07" value="opi_07"><a>10:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_08" value="opi_08"><a>11:30h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="opi_09" value="opi_09"><a>12:30h </a> Estética facial</label>
                     </div>
                     <div>
-                        <p>Consulta opi:</p>
-                        <label><input type="checkbox" name="registro[]" id="salaopi_01" value="salaopi_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="salaopi_02" value="salaopi_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="salaopi_03" value="salaopi_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="salaopi_04" value="salaopi_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="salaopi_05" value="salaopi_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Miércoles</p>
+                        <label><input type="checkbox" name="registro[]" id="salaopi_01" value="salaopi_01"><a>10:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="salaopi_02" value="salaopi_02"><a>11:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="salaopi_03" value="salaopi_03"><a>12:30h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="salaopi_04" value="salaopi_04"><a>13:30h </a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="salaopi_05" value="salaopi_05"><a>16:30h  </a> Limpieza de Cutis</label>
                     </div>
                 </div>
                 <!--#opi-->
                 <div id="peluqueria" class="contenido-producto clearfix">
                     <h4>Peluqería</h4>
                     <div>
-                        <p>Consulta 1</p>
-                        <label><input type="checkbox" name="registro[]" id="pel_01" value="pel_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_02" value="pel_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_03" value="pel_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_05" value="pel_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_05" value="pel_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Lunes</p>
+                        <label><input type="checkbox" name="registro[]" id="pel_01" value="pel_01"><a>10:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_02" value="pel_02"><a>11:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_03" value="pel_03"><a>13:30h</a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_05" value="pel_04"><a>15:30h</a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_05" value="pel_05"><a>16:30h </a> Limpieza de Cutis</label>
                     </div>
                     <div>
-                        <p>Consulta 2:</p>
-                        <label><input type="checkbox" name="registro[]" id="pel_06" value="pel_06"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_07" value="pel_07"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_08" value="pel_08"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="pel_09" value="pel_09"><price>55€ - 300€ </price> Estética facial</label>
+                        <p>Martes</p>
+                        <label><input type="checkbox" name="registro[]" id="pel_06" value="pel_06"><a>11:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_07" value="pel_07"><a>14:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_08" value="pel_08"><a>16:30h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="pel_09" value="pel_09"><a>17:20h </a> Estética facial</label>
                     </div>
                     <div>
-                        <p>Consulta 3:</p>
-                        <label><input type="checkbox" name="registro[]" id="salapel_01" value="salapel_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="salapel_02" value="salapel_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="salapel_03" value="salapel_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="salapel_04" value="salapel_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="salapel_05" value="salapel_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Miércoles</p>
+                        <label><input type="checkbox" name="registro[]" id="salapel_01" value="salapel_01"><a>16:30h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="salapel_02" value="salapel_02"><a>17:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="salapel_03" value="salapel_03"><a>18:10h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="salapel_04" value="salapel_04"><a>18:50h </a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="salapel_05" value="salapel_05"><a>19:15h  </a> Limpieza de Cutis</label>
                     </div>
                 </div>
                 <!--#peluqueria-->
                 <div id="promociones" class="contenido-producto clearfix">
                     <h4>Promociones Web</h4>
                     <div>
-                        <p>Consulta 1</p>
-                        <label><input type="checkbox" name="registro[]" id="prom_01" value="prom_02"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_02" value="prom_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_03" value="prom_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_04" value="prom_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_05" value="prom_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Lunes</p>
+                        <label><input type="checkbox" name="registro[]" id="prom_01" value="prom_02"><a>09:50h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_02" value="prom_02"><a>10:15h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_03" value="prom_03"><a>11:45h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_04" value="prom_04"><a>15:50h </a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_05" value="prom_05"><a>16:45h  </a> Limpieza de Cutis</label>
                     </div>
                     <div>
-                        <p>Consulta 2:</p>
-                        <label><input type="checkbox" name="registro[]" id="prom_06" value="prom_06"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_07" value="prom_07"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_08" value="prom_08"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="prom_09" value="prom_09"><price>55€ - 300€ </price> Estética facial</label>
+                        <p>Martes</p>
+                        <label><input type="checkbox" name="registro[]" id="prom_06" value="prom_06"><a>15:50h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_07" value="prom_07"><a>16:30h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_08" value="prom_08"><a>17:50h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="prom_09" value="prom_09"><a>19h </a> Estética facial</label>
                     </div>
                     <div>
-                        <p>Consulta prom:</p>
-                        <label><input type="checkbox" name="registro[]" id="salaprom_01" value="salaprom_01"><price>3,00€ - 18,90€</price> Depilación Hilo y Cera</label>
-                        <label><input type="checkbox" name="registro[]" id="salaprom_02" value="salaprom_02"><price>75€ - 375€</price> Depilación Láser Diodo</label>
-                        <label><input type="checkbox" name="registro[]" id="salaprom_03" value="salaprom_03"><price>35€ - 175€ </price> Estética Corporal</label>
-                        <label><input type="checkbox" name="registro[]" id="salaprom_04" value="salaprom_04"><price>55€ - 300€ </price> Estética facial</label>
-                        <label><input type="checkbox" name="registro[]" id="salaprom_05" value="salaprom_05"><price>40€ - 120€  </price> Limpieza de Cutis</label>
+                        <p>Miércoles</p>
+                        <label><input type="checkbox" name="registro[]" id="salaprom_01" value="salaprom_01"><a>13:00h</a> Depilación Hilo y Cera</label>
+                        <label><input type="checkbox" name="registro[]" id="salaprom_02" value="salaprom_02"><a>16:00h</a> Depilación Láser Diodo</label>
+                        <label><input type="checkbox" name="registro[]" id="salaprom_03" value="salaprom_03"><a>17:00h </a> Estética Corporal</label>
+                        <label><input type="checkbox" name="registro[]" id="salaprom_04" value="salaprom_04"><a>18:00h</a> Estética facial</label>
+                        <label><input type="checkbox" name="registro[]" id="salaprom_05" value="salaprom_05"><a>19:00h  </a> Limpieza de Cutis</label>
                     </div>
                 </div>
             </div>
